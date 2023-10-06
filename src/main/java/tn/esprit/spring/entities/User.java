7a3de2d008123d0d91a0e0f514c75c5a328301ef
safafliss <safa.fliss@esprit.tn>
2023-10-06 15:00:52 +0100

    second commit

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -14,6 +14,8 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+//hello
+
 @Entity
 @Table(name = "T_USER")
 public class User implements Serializable {
